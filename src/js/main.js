6b6e8cb3ede47c02802ac1285e4acefdaeb38055
AndresHernandez <pipehernandez1998@gmail.com>
2024-04-26 13:53:50 -0500

    se avanza en ejercicio

---
+++ src/js/main.js
@@ -0,0 +1,145 @@
+// SISTEMA DE GESTIÒN PARA VETERINARIA
+
+//CREAMOS UN ARREGLO Y DENTRO DEL ARREGLO CREAMOS UN OBJETO CON LA INFORMACIÒN DE CADA MASCOTA. 
+
+let mascotas = [
+    {
+        nombre: "Aròn",
+        especie: "Perro",
+        raza: "Criollo",
+        edad: 1,
+        peso: 20,
+        estado: "estable",
+        nombrePropietario: "Simòn Arias",
+        documentoPropietario: "123456789",
+        telefonoPropietario: "123456789",
+        correoPropietario: "sariass077@gmail.com"
+    },
+    {
+        nombre: "Flofy",
+        especie: "Perro",
+        raza: "Labrador",
+        edad: 3,
+        peso: 25,
+        estado: "crítico",
+        nombrePropietario: "Camila Perez",
+        documentoPropietario: "987654321",
+        telefonoPropietario: "987654321",
+        correoPropietario: "camila08perez@gmail.com"
+    },
+    {
+        nombre: "Michi",
+        especie: "Gato",
+        raza: "Siamés",
+        edad: 2,
+        peso: 12,
+        estado: "estable",
+        nombrePropietario: "Carlos Pérez",
+        documentoPropietario: "567890123",
+        telefonoPropietario: "567890123",
+        correoPropietario: "carlosperez@gmail.com"
+    },
+    {
+        nombre: "Paco",
+        especie: "Perro",
+        raza: "Golden Retriever",
+        edad: 4,
+        peso: 30,
+        estado: "crítico",
+        nombrePropietario: "Ana Martínez",
+        documentoPropietario: "234567890",
+        telefonoPropietario: "234567890",
+        correoPropietario: "anamartinez@gmail.com"
+    },
+    {
+        nombre: "Scar",
+        especie: "Gato",
+        raza: "Persa",
+        edad: 5,
+        peso: 15,
+        estado: "estable",
+        nombrePropietario: "María Rodríguez",
+        documentoPropietario: "345678901",
+        telefonoPropietario: "345678901",
+        correoPropietario: "mariarodriguez@gmail.com"
+    },
+    {
+        nombre: "Rufo",
+        especie: "Perro",
+        raza: "Bulldog Francés",
+        edad: 2,
+        peso: 18,
+        estado: "crítico",
+        nombrePropietario: "Juan Pérez",
+        documentoPropietario: "456789012",
+        telefonoPropietario: "456789012",
+        correoPropietario: "juanperez@gmail.com"
+    },
+    {
+        nombre: "Caos",
+        especie: "Perro",
+        raza: "Chihuahua",
+        edad: 2,
+        peso: 5,
+        estado: "estable",
+        nombrePropietario: "Luisa Gómez",
+        documentoPropietario: "890123456",
+        telefonoPropietario: "890123456",
+        correoPropietario: "luisagomez@gmail.com"
+    },
+    {
+        nombre: "Linda",
+        especie: "Gato",
+        raza: "Maine Coon",
+        edad: 3,
+        peso: 20,
+        estado: "crítico",
+        nombrePropietario: "Elena Martínez",
+        documentoPropietario: "901234567",
+        telefonoPropietario: "901234567",
+        correoPropietario: "elenamartinez@gmail.com"
+    },
+    {
+        nombre: "Trosky",
+        especie: "Perro",
+        raza: "Dálmata",
+        edad: 1,
+        peso: 22,
+        estado: "estable",
+        nombrePropietario: "Lucía Rodríguez",
+        documentoPropietario: "789012345",
+        telefonoPropietario: "789012345",
+        correoPropietario: "luciarodriguez@gmail.com"
+    },
+    {
+        nombre: "Kendra",
+        especie: "Gato",
+        raza: "Siamesa",
+        edad: 4,
+        peso: 14,
+        estado: "crítico",
+        nombrePropietario: "Lucía Rodríguez",
+        documentoPropietario: "789012345",
+        telefonoPropietario: "789012345",
+        correoPropietario: "luciarodriguez@gmail.com"
+    }
+];
+
+// EL SISTEMA DEBE PERMITIR REALIZAR OPERACONES CRUD (crear, leer, actualizar, eliminar)
+
+function agregarMascota(arrayMascotas, nombre, especie, raza, edad, peso, estado, nombrePropietario, documentoPropietario, telefonoPropietario, correoPropietario) {
+
+
+}
+
+function leer(params) {
+
+}
+
+function actualizar(params) {
+
+}
+
+function eliminar(params) {
+
+}
